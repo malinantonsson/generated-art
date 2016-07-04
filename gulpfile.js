54acd2190bc28877f7670262bb3217c3ffcdf874
Malin Antonsson <Malin.Antonsson@rga.com>
2016-07-04 21:34:10 +0100

    more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 /* libs */
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-//var _ = require('underscore');
-var extender = require('gulp-html-extend');
-var watch = require('gulp-watch');
-var batch = require('gulp-batch');
 var ignore = require('gulp-ignore');
-//var template = require('gulp-template');
-var rename = require('gulp-rename');
-var shell = require('gulp-shell');
-var imageResize = require('gulp-image-resize');
-var rimraf = require('gulp-rimraf');
-var sizeOf = require('image-size');
 var runSequence = require('run-sequence');
-var svgmin = require('gulp-svgmin');
-var Q = require('q');
 
+var $ = require('gulp-load-plugins')();
+var lazypipe = require('lazypipe');
+var del = require('del');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 var nunjucksRender = require('gulp-nunjucks-render');
 var sass = require('gulp-sass');
+var argv = require('yargs').argv;
+
+var plugins = require("gulp-load-plugins")({
+    pattern: ['gulp-*', 'gulp.*'],
+    replaceString: /\bgulp[\-.]/
+});
 
 /* vars */
 var appPath = './src';
@@ -26,15 +27,66 @@ var distPath = './dist/';
 var sassSrc = appPath + '/styles/*.scss';
 var sassDist = distPath + '/css/';
 
+var jsSrc = appPath + '/js/*.js';
+var jsDist = distPath + '/js/';
+
 var templatesSrc = appPath + '/templates/';
 var partialsSrc = templatesSrc + '/partials/';
 var pagesPath = appPath + '/pages/**/*.nunjucks';
 var pagesDist = distPath;
 
-gulp.task('sass', function () {
-  return gulp.src(sassSrc)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(sassDist));
+var config = {
+  defaultPort: 3000,
+  supportedBrowsers: [
+    'ie >= 9',
+    'last 1 Firefox versions',
+    'last 1 Chrome versions',
+    'Safari >= 6',
+    'iOS >= 6',
+    'ChromeAndroid >= 4.2'
+  ],
+  version: require('./package.json').version,
+  minify: argv.minify || false
+};
+
+// Clean site directory
+gulp.task('clean', del.bind(null, [distPath], {dot: true}));
+
+
+gulp.task('styles', function() {
+  gulp.src(sassSrc)
+  	.pipe(sourcemaps.init()) // Initialize sourcemap plugin
+    .pipe(sass()) 
+    .pipe(autoprefixer()) // Passes it through gulp-autoprefixer 
+    .pipe(sourcemaps.write()) // Writing sourcemaps 
+    .pipe(gulp.dest(sassDist)) // Outputs it in the css folder
+    // Reloading the stream
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+var scriptsFinish = lazypipe()
+  .pipe(gulp.dest, jsDist)
+  .pipe(function () {
+    return $.if(config.minify, $.uglify());
+  })
+  .pipe(function () {
+    return $.if(config.minify, $.rename({suffix: '.min'}));
+  })
+  .pipe(function () {
+    return $.if(config.minify, gulp.dest(jsDist));
+  });
+
+// Lint and build scripts
+gulp.task('scripts', function() {
+  return gulp.src(jsSrc)
+    .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
+    .pipe($.if(config.isWatching, $.jshint()))
+    .pipe($.if(config.isWatching, $.jshint.reporter('jshint-stylish')))
+    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')))
+    .pipe($.concat('scripts.js'))
+    .pipe(scriptsFinish());
 });
  
 gulp.task('nunjucks', function() {
@@ -46,4 +98,36 @@ gulp.task('nunjucks', function() {
     }))
   // output files in app folder
   .pipe(gulp.dest(pagesDist))
+});
+
+gulp.task('setWatch', function() {
+  config.isWatching = true;
+});
+
+// Development task
+gulp.task('dev', ['default', 'setWatch'], function() {
+  browserSync({
+    port: argv.port || config.defaultPort, //default: 3000
+    server: { baseDir: distPath},
+    ui: {
+      port: argv.port + 5000 || config.defaultPort + 5000, //default: 8000
+      weinre: { port: argv.port + 6092 || config.defaultPort + 6092 } //default: 9092
+    },
+    notify: false,
+    logLevel: 'silent' //other oprions: info, debug
+  });
+
+  gulp.watch([sassSrc], ['styles', reload]);
+  gulp.watch([templatesSrc + '**/*.nunjucks'], ['nunjucks', reload]);
+  gulp.watch([pagesPath], ['nunjucks', reload]);
+  gulp.watch([jsSrc], ['scripts', reload]);
+});
+
+// Build production files, the default task
+gulp.task('default', ['clean'], function (cb) {
+  runSequence([
+      'styles',
+      'scripts',
+      'nunjucks'
+    ], cb);
 });
